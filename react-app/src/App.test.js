bb903f96e1629a1dee94b040cec6c80303aa1ac3
Mauricio Quiroga <mauricio.quiroga@3xmgroup.com>
2019-10-28 10:25:14 -0300

    Add tests

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('Correctly displays initial message', () => {
+  const { getByText } = render(<App />);
+  const element = getByText('Click the button below to log in')
+  expect(element).toBeInTheDocument();
+});
+
+it('Displays login button', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Login')).toBeInTheDocument();
+});
+
+it('Displays enabled login button', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Login')).toBeEnabled();
+});
+
+it('Disables login button after clicking it', () => {
+  const { getByText } = render(<App />);
+  const loginButton = getByText('Login');
+  fireEvent.click(loginButton);
+  expect(loginButton).toBeDisabled();
+});
+
+it('Shows "welcome" message after loggin in ', async () => {
+  const { container, getByText, getByTestId } = render(<App />);
+  const loginButton = getByText('Login');
+  fireEvent.click(loginButton);
+  const welcomeMessageElement = await waitForElement(() => getByTestId('welcome-message'), { container });
+  expect(welcomeMessageElement).toBeInTheDocument();
+  expect(welcomeMessageElement.textContent).toBe('Welcome');
 });
